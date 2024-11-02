527124753a92aa53ce28bd519097f6d0fa5b2932
aarya-kumar <aarya.kumar1712@gmail.com>
2024-11-02 13:09:47 +0530

    setting up employee and admin page after login

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react'
+import { getLocalStorage } from '../utils/localStorage'
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+
+    const [userData, setUserData] = useState(null)
+
+    setUserData(getLocalStorage())
+
+  return (
+    <div>
+      <AuthContext.Provider value={"Aarya"}>
+        {children}
+      </AuthContext.Provider>
+    </div>
+  )
+}
+
+export default AuthProvider
